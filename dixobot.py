13c6358383517f1f51a52873abc03c2262ff4110
Dmytro <dmytro.nebelskyi@gmail.com>
2023-05-05 12:02:37 +0300

    menu added

--- dixobot.py
+++ dixobot.py
@@ -2,10 +2,12 @@ import logging
 
 from steam_community_market import Market, AppID
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from config import TOKEN_API
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token='6089352897:AAFJC_LSB7jhF1MO9-Xu24J576-wSJtW7VQ')
+bot = Bot(token=TOKEN_API)
 dp = Dispatcher(bot)
 
 market = Market("UAH")
@@ -13,23 +15,34 @@ market = Market("UAH")
 sticker = "Sticker | Natus Vincere (Gold) | Paris 2023"
 awp = "AWP | Chromatic Aberration (Minimal Wear)"
 
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Hey there ! I am d1xonella bot. This is Beta.v")
+menu = ReplyKeyboardMarkup(resize_keyboard=True).add(
+    KeyboardButton('Get Sticker Price'),
+    KeyboardButton('Get AWP Price')
+)
 
-@dp.message_handler(commands=['stickerprice'])
 async def send_stickerprice(message: types.Message):
     await message.answer((sticker + ": "+ str(market.get_lowest_price(sticker, AppID.CSGO)) + " UAH"))
     await message.delete()
 
-@dp.message_handler(commands=['awpprice'])
 async def send_awpprice(message: types.Message):
     await message.answer((awp + ": " + str(market.get_lowest_price(awp, AppID.CSGO)) + " UAH"))
     await message.delete()
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.reply(message.text)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hey there ! I am d1xonella bot. This is Beta.v")
+
+@dp.message_handler(content_types=['text'])
+async def process_message(message: types.Message):
+    # Если пользователь нажал на кнопку "Стоимость стикера"
+    if message.text == 'Get Sticker Price':
+        await send_stickerprice(message)
+    # Если пользователь нажал на кнопку "Стоимость AWP"
+    elif message.text == 'Get AWP Price':
+        await send_awpprice(message)
+    # Если пользователь отправил другое сообщение, отправляем ему меню
+    else:
+        await message.answer('Select what do you want to get:', reply_markup=menu)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
