a24bbb12afc34a817f1269349f8260528aba01aa
Dmytro <dmytro.nebelskyi@gmail.com>
2023-05-05 09:46:36 +0300

    Small changes

--- dixobot.py
+++ dixobot.py
@@ -3,30 +3,27 @@ import logging
 from steam_community_market import Market, AppID
 from aiogram import Bot, Dispatcher, executor, types
 
-# установить уровень логирования
 logging.basicConfig(level=logging.INFO)
 
-# инициализировать бота и диспетчер
 bot = Bot(token='6089352897:AAFJC_LSB7jhF1MO9-Xu24J576-wSJtW7VQ')
 dp = Dispatcher(bot)
 
 market = Market("UAH")
 
-item = "AWP | Chromatic Aberration (Field-Tested)"
+sticker = "Sticker | Natus Vincere (Gold) | Paris 2023"
+awp = "AWP | Chromatic Aberration (Minimal Wear)"
 
-# хэндлер на команду /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    """
-    Этот хэндлер будет вызываться, когда пользователь введет команду /start.
-    Он отправит приветственное сообщение с инструкциями.
-    """
     await message.reply("Hey there ! I am d1xonella bot. This is Beta.v")
 
-# хэндлер на команду /help
-@dp.message_handler(commands=['price'])
+@dp.message_handler(commands=['stickerprice'])
 async def send_help(message: types.Message):
-    await message.reply(market.get_lowest_price(item, AppID.CSGO))
+    await message.reply(sticker + ": " + market.get_lowest_price(sticker, AppID.CSGO) + "UAH")
+
+@dp.message_handler(commands=['awpprice'])
+async def send_help(message: types.Message):
+    await message.reply(awp + ":" + market.get_lowest_price(awp, AppID.CSGO) + "UAH")
 
 # хэндлер на любое сообщение
 @dp.message_handler()
